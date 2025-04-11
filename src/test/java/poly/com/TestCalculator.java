1b0bc612dea108e845fa0fd8fd86f957792b7090
Khoanhps39175 <Khoanhps39175@gmail.com>
2025-04-12 00:49:50 +0700

    Test fail

---
+++ src/test/java/poly/com/TestCalculator.java
@@ -0,0 +1,19 @@
+package poly.com;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCalculator {
+
+  @Test
+  public void testSum() {
+    Calculator calculator = new Calculator();
+    Assert.assertEquals(calculator.sum(2, 3), 5);
+  }
+
+  @Test
+  public void testSub() {
+    Calculator calculator = new Calculator();
+    Assert.assertEquals(calculator.sub(2, 2), 0);
+  }
+}

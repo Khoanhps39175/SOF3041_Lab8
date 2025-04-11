937b7d4489a08f8fcad0455796f5f899df20e2a5
Khoanhps39175 <Khoanhps39175@gmail.com>
2025-04-12 00:40:17 +0700

    First commit

---
+++ src/main/java/poly/com/Calculator.java
@@ -0,0 +1,11 @@
+package poly.com;
+
+public class Calculator implements Icaculator{
+	public Integer sum(Integer a, Integer b) {
+		return a+b;
+	}
+	
+	public Integer sub(Integer a, Integer b) {
+		return a-b;
+	}
+}
---
+++ src/main/java/poly/com/Icaculator.java
@@ -0,0 +1,6 @@
+package poly.com;
+
+public interface Icaculator {
+	Integer sum(Integer a, Integer b);
+	Integer sub(Integer a, Integer b);
+}

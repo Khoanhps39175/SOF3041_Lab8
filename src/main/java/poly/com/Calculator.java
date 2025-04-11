1b25e7bcf374eebe3709f2b026263952bd111c3d
Khoanhps39175 <Khoanhps39175@gmail.com>
2025-04-12 00:54:23 +0700

    Test fail

--- src/main/java/poly/com/Calculator.java
+++ src/main/java/poly/com/Calculator.java
@@ -2,10 +2,10 @@ package poly.com;
 
 public class Calculator implements Icaculator{
 	public Integer sum(Integer a, Integer b) {
-		return a+b;
+		return 0;
 	}
 	
 	public Integer sub(Integer a, Integer b) {
-		return a-b;
+		return 0;
 	}
 }
